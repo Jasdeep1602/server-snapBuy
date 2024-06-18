5187d0a9a8ae18af6289f51c7b2b20da68a1bce8
Jasdeep2k15 <singhjasdeep1602@gmail.com>
2024-06-19 01:00:23 +0530

    check auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const auth = (req, res, next) => {
   try {
     const token = req.header('Authorization');
-    if (!token) return res.status(400).json({ msg: 'Invalid Authentication' });
+    if (!token) return res.status(400).json({ msg: 'Invalid Authenticatio' });
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) return res.status(400).json({ msg: 'Invalid Authentication' });
